7c6ef72b20552697a0ca0fc2d6b6acbb730026b4
Hnnrik <113845766+Hnnrik@users.noreply.github.com>
2024-04-04 21:23:10 -0300

    upando outras coisas

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,15 +6,39 @@ const Jogo = require('../models/jogoModel');
 const Usuario = require('../models/usuarioModel');
 
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'home.html')); 
+router.get('/', async (req, res) => {
+  try {
+    const jogos = await Jogo.find();
+
+    res.render('home', { jogos });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao recuperar os jogos.');
+  }
 });
 
-router.get('/biblioteca', (req, res) => {
-  res.sendFile(path.join(__dirname, '..','views', 'biblioteca.html'));
+router.get('/biblioteca', async (req, res) => {
+  try {
+    const jogos = await Jogo.find();
+
+    res.render('biblioteca', { jogos });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao recuperar os jogos.');
+  }
 });
 
-router.get('/cadastro-jogo', (req, res) => {
+
+function verificaAutenticacao(req, res, next) {
+  if (req.session.usuario) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+
+router.get('/cadastro-jogo', verificaAutenticacao ,(req, res) => {
   res.sendFile(path.join(__dirname,'..', 'views', 'cadastro-jogo.html'));
 });
 
@@ -33,13 +57,59 @@ router.get('/sobre', (req, res) => {
 router.get('/tela-jogo', (req, res) => {
   res.sendFile(path.join(__dirname, '..','views', 'tela-jogo.html'));
 });
+
+
 router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '..','views','login.html'));
 });
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, senha } = req.body;
+
+    const usuario = await Usuario.findOne({ email, senha });
+    if (!usuario) {
+      return res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+    
+    res.redirect('/');
+    //res.status(200).json({ message: 'Login bem-sucedido', usuario });
+  } catch (error) {
+    console.error('Erro ao fazer login:', error);
+    res.status(500).json({ message: 'Erro ao fazer login' });
+  }
+});
+
+
 router.get('/registrar', (req, res) => {
   res.sendFile(path.join(__dirname, '..','views','registrar.html'));
 });
 
+router.post('/registrar', async (req, res) => {
+  try {
+    const { nome, nomeDeUsuario, senha, email, dataNascimento } = req.body;
+
+    const usuarioExistente = await Usuario.findOne({ $or: [{ nomeDeUsuario }, { email }] });
+    if (usuarioExistente) {
+      return res.status(400).json({ message: 'Nome de usuário ou e-mail já está em uso.' });
+    }
+
+    const novoUsuario = new Usuario({
+      nome,
+      nomeDeUsuario,
+      senha,
+      email,
+      dataNascimento
+    });
+
+    // Salvar o novo usuário no banco de dados
+    const usuarioSalvo = await novoUsuario.save();
+    res.status(201).json(usuarioSalvo);
+  } catch (error) {
+    console.error('Erro ao registrar usuário:', error);
+    res.status(500).json({ message: 'Erro ao registrar usuário.' });
+  }
+});
 router.post('/post-jogo', async (req, res) => {
   try {
     const { nome, imagem,descricao,nota } = req.body;
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const session = require('express-session');
 const controllerRoutes = require('./controllers/userController'); 
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs'); // Defina o engine de templates como EJS
+app.set('views', path.join(__dirname, 'views')); // Defina o diretório de views
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,6 +23,12 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 .then(() => console.log('Conectado ao MongoDB Atlas'))
 .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
+
+app.use(session({
+  secret: 'segredo', // Uma chave secreta para assinar os cookies de sessão
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
